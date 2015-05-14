14890bb98ffb4e7ca522ec6d6bc28f3d5dfc90cf
Daniel Carroza <danielcarrozasantana@gmail.com>
2015-05-14 10:18:36 +0200

    first commit

---
+++ src/main/scala/com/stratio/receiver/RabbitMQInputDStream.scala
@@ -0,0 +1,121 @@
+/**
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stratio.receiver
+
+import java.net.ConnectException
+import java.util
+
+import com.rabbitmq.client.{Channel, Connection, ConnectionFactory, QueueingConsumer}
+import org.apache.spark.Logging
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.dstream.ReceiverInputDStream
+import org.apache.spark.streaming.receiver.Receiver
+
+
+private[receiver]
+class RabbitMQInputDStream(
+                         @transient ssc_ : StreamingContext,
+                         rabbitMQQueueName: Option[String],
+                         rabbitMQHost: String,
+                         rabbitMQPort: Int,
+                         exchangeName: Option[String],
+                         routingKeys: Seq[String],
+                         storageLevel: StorageLevel
+                         ) extends ReceiverInputDStream[String](ssc_) with Logging {
+
+  override def getReceiver(): Receiver[String] = {
+    val DefaultRabbitMQPort = 5672
+
+    new RabbitMQReceiver(rabbitMQQueueName,
+      Some(rabbitMQHost).getOrElse("localhost"),
+      Some(rabbitMQPort).getOrElse(DefaultRabbitMQPort),
+      exchangeName,
+      routingKeys,
+      storageLevel)
+  }
+}
+
+private[receiver]
+class RabbitMQReceiver(rabbitMQQueueName: Option[String],
+                       rabbitMQHost: String,
+                       rabbitMQPort: Int,
+                       exchangeName: Option[String],
+                       routingKeys: Seq[String],
+                       storageLevel: StorageLevel)
+  extends Receiver[String](storageLevel) with Logging {
+
+  val DirectExchangeType: String = "direct"
+
+  def onStart() {
+    implicit val akkaSystem = akka.actor.ActorSystem()
+    receive()
+  }
+
+  def onStop() {
+    // There is nothing much to do as the thread calling receive()
+    // is designed to stop by itself isStopped() returns false
+  }
+
+  /** Create a socket connection and receive data until receiver is stopped */
+  private def receive() {
+
+    try {
+      val factory: ConnectionFactory = new ConnectionFactory
+      factory.setHost(rabbitMQHost)
+      factory.setPort(rabbitMQPort)
+      val connection: Connection = factory.newConnection
+      val channel: Channel = connection.createChannel
+
+      var queueName = ""
+      if (!routingKeys.isEmpty){
+        channel.exchangeDeclare(exchangeName.get, DirectExchangeType)
+        queueName = channel.queueDeclare().getQueue()
+
+        for (routingKey: String <- routingKeys) {
+          channel.queueBind(queueName, exchangeName.get, routingKey)
+        }
+      }else{
+        channel.queueDeclare(rabbitMQQueueName.get, false, false, false, new util.HashMap(0))
+        queueName = rabbitMQQueueName.get
+      }
+
+      log.error("RabbitMQ Input waiting for messages")
+      val consumer: QueueingConsumer = new QueueingConsumer(channel)
+      channel.basicConsume(queueName, true, consumer)
+      while (!isStopped) {
+        val delivery: QueueingConsumer.Delivery = consumer.nextDelivery
+        store(new String(delivery.getBody))
+      }
+
+      log.info("it has been stopped")
+      channel.close
+      connection.close
+      restart("Trying to connect again")
+    }
+    catch {
+      case ce: ConnectException => {
+        log.error("Could not connect")
+        restart("Could not connect", ce)
+      }
+      case t: Throwable => {
+        log.error("Error receiving data")
+        restart("Error receiving data", t)
+      }
+    }
+  }
+}
